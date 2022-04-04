46f68be19aba17a2cb9458b7c008e4e306825ae3
Ynifa <yasine.nifa@gmail.com>
2022-04-04 22:37:20 +0200

    Add toggle button to show and hide a div
    Signed-off-by: Ynifa <yasine.nifa@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,84 @@
+import { NavLink } from "react-router-dom";
+import { FaBars, FaHome, FaLock, FaMoneyBill, FaUser } from "react-icons/fa";
+import { MdMessage } from "react-icons/md";
+import { BiAnalyse, BiSearch } from "react-icons/bi";
+import { BiCog } from "react-icons/bi";
+import { AiFillHeart, AiTwotoneFileExclamation } from "react-icons/ai";
+import { BsCartCheck } from "react-icons/bs";
+import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import './Sidebar.css'
+function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const inputAnimation = {
+    hidden: {
+      width: 0,
+      padding: 0,
+      transition: {
+        duration: 0.2,
+      },
+    },
+    show: {
+      width: "140px",
+      padding: "5px 15px",
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
+
+  const showAnimation = {
+    hidden: {
+      width: 0,
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    show: {
+      opacity: 1,
+      width: "auto",
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
+  return (
+    <div className="main-container">
+      <motion.div
+        animate={{
+          width: isOpen ? "300px" : "50px",
+          transition: {
+            duration: 0.5,
+            type: "spring",
+            damping: 10,
+          },
+        }}
+        className="sidebar"
+      >
+        <div className="top-section">
+          <AnimatePresence>
+            {isOpen && (
+              <motion.h1
+                variants={showAnimation}
+                initial="hidden"
+                animate="show"
+                exit="hidden"
+                className="logo"
+              >
+                Dashboard
+              </motion.h1>
+            )}
+          </AnimatePresence>
+          <div className="bars">
+            <FaBars onClick={toggle} />
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
+}
+
+export default Sidebar;
